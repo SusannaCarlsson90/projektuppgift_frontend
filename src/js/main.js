83554d7b589acc0ba70180b89b4af180a8a15f23
SusannaCarlsson90 <susanna.carlsson90@icloud.com>
2026-03-12 12:00:33 +0100

    la till JSDoc på det jag skapat hittills

--- src/js/main.js
+++ src/js/main.js
@@ -10,7 +10,14 @@ if (hamburger && navMeny) {
   });
 }
 
+/** @type {Array} En lista som lagrar alla hämtade recept från API:et */
+
 let allDesserts = []; //Skapar variabel för att lagra alla desserter från API i. Tar emot som en tom Array.
+
+/**
+ * Hämtar receptdata från Spoonacular API baserat på ett sökord.
+ * @param {string} query - Ordet som användaren söker efter
+ */
 
 async function loadData(query) {
   //Funktion som heter data i ett JSON-format som görs om till ett JavaScript objekt
@@ -31,16 +38,27 @@ async function loadData(query) {
   }
 }
 
+/** @type {HTMLElement} - Knappen som användaren klickar på för att söka */
 const searchBtn = document.getElementById("searchBtn");
+
+/** @type {HTMLElement} - Textfältet där användaren skriver in sitt sökord */
 const inputText = document.getElementById("recipeSearch");
+
+/** @type {HTMLElement} - "Behållaren" där receptkorten ska skrivas ut */
 const recipeHome = document.getElementById("recipeHome");
+
+/**
+ * Hämtar recept från API:et och sparar dem i listan
+ * @param {Array} recipes - Listan med alla recept som ska visas
+ */
 
 function displayDesserts(recipes) {
   //Funktion som skriver ut recepten till min index.html
-  recipeHome.innerHTML = ""; //Tömmer sidan på gamla recept innan nya ritas ut;
+  recipeHome.innerHTML = ""; //Tömmer sidan på gamla recept innan nya ritas ut
   recipes.forEach((recipe) => {
+    //för varje recept gör detta:
     const recipeCard = `
-    <div class="recipeCard">
+    <div class="recipeCard">  
       <h3>${recipe.title}</h3>
       <img src="${recipe.image}" alt="${recipe.title}">
 
@@ -51,22 +69,25 @@ function displayDesserts(recipes) {
 }
 
 searchBtn.addEventListener("click", () => {
+  //eventlyssnare vid klick på sökknappen
   const query = inputText.value;
 
   if (query !== "") {
+    //Om sökfältet inte är tomt kör loadData
     loadData(query);
   } else {
-    alert("Du måste skriva något att söka efter");
+    alert("Du måste skriva något att söka efter"); //Annars kör popupruta med denna text
   }
 });
 
 inputText.addEventListener("keydown", (event) => {
+  //eventlyssnare vid tryck på enter
   if (event.key === "Enter") {
     const query = inputText.value;
     if (query !== "") {
-      loadData(query);
+      loadData(query); //Om inputvärdet från användaren inte är tomt kör loadData
     } else {
-      alert("Du måste skriva något att söka efter");
+      alert("Du måste skriva något att söka efter"); //Annars popupruta med texten
     }
   }
 });
