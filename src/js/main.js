807b3f75a3ccef35e018bb83624343ec6ce7b576
SusannaCarlsson90 <susanna.carlsson90@icloud.com>
2026-03-12 09:24:43 +0100

    skapat funktion som hämtar recept från Spoonacular API

--- src/js/main.js
+++ src/js/main.js
@@ -10,19 +10,24 @@ if (hamburger && navMeny) {
   });
 }
 
-async function loadData() {
-  const url = "https://api.spoonacular.com/recipes/complexSearch";
-  //anropa och läs ut data
+let allDesserts = []; //Skapar variabel för att lagra alla desserter från API i. Tar emot som en tom Array.
+
+async function loadData(query) {
+  //Funktion som heter data i ett JSON-format som görs om till ett JavaScript objekt
+  const apiKey = "39499d580b344dc5bafd2e88b22e6afb";
+  const url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${query}&number=50`; //Min URL där jag vill ha sökord från användaren och hämta 50 resultat i taget.
 
   try {
     const response = await fetch(url);
-    allCourses = await response.json();
+    const data = await response.json(); //Packar upp "paketet" från servern
+    allDesserts = data.results; //Sparar receptlistan i min globala variabel
 
     //Lagra globalt
-    console.table(allCourses);
-
-    displayCourses(allCourses);
+    console.table(allDesserts);
+    //displayDesserts(allDesserts);
   } catch (error) {
     console.error("Fel: " + error);
   }
 }
+
+loadData("brownies"); //Testsökord
