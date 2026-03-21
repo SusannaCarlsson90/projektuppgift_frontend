6a12c421f62bd3cc24a41a82ea4b41375ea01c7d
SusannaCarlsson90 <susanna.carlsson90@icloud.com>
2026-03-21 17:33:53 +0100

    la till om och bonus i min vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,8 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
+        om: resolve(__dirname, "om.html"),
+        bonus: resolve(__dirname, "bonus.html"),
       },
     },
   },
